eb5b298b626eb7c4a486928d1b189792f3d17b26
Andrius Svylas <asvylas@gmail.com>
2017-12-07 11:26:27 +0200

    Add files via upload

---
+++ autoXML.ps1
@@ -0,0 +1,22 @@
+﻿[xml]$xmlData = get-content C:\Users\svylasan\Desktop\data.xml
+[xml]$xmlMastersheet = get-content C:\Users\svylasan\Desktop\mastersheet.xml
+    
+    for($i=0; $i-lt $xmlData.INSTAT.Envelope.Declaration.Item.CN8.CN8Code.Length; $i++)
+    {
+        for ($z=0; $z-lt $xmlMastersheet.root.row.Length; $z++)
+        {
+            if($xmlMastersheet.root.row.codenumber[$z] -eq $xmlData.INSTAT.Envelope.Declaration.Item.CN8.CN8Code[$i])
+            {
+            $xmlData.INSTAT.Envelope.Declaration.Item[$i].goodsDescription = $xmlMastersheet.root.row.desc[$z]
+            echo $xmlData.INSTAT.Envelope.Declaration.Item[$i].goodsDescription
+            }
+        }
+
+    }
+    
+$xmlData.save("C:\Users\svylasan\Desktop\File.xml")
+echo ""
+echo ""
+echo "TASK COMPLETED!"
+echo ""
+echo ""
